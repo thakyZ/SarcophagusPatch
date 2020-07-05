91fdd4164aeb5b48a075f6a485c94cffef10f391
thakyZ <thakyz@outlook.com>
2020-07-04 17:51:21 -0700

    Initial Commit

---
+++ Source/SarcophagusPatch.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using HarmonyLib;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+using UnityEngine;
+using System.Linq;
+
+namespace NekoBoiNick.SarcophagusPatch
+{
+  [StaticConstructorOnStartup]
+  class Main
+  {
+    static Main()
+    {
+      new Harmony("nekoboinick.vanilla.sarcophaguspatch").PatchAll(Assembly.GetExecutingAssembly());
+      Log.Message("Vanilla Sarcophagus Patch initialized. This mod uses Harmony (all patches are destructive): RimWorld.QualityUtility.SendCraftNotification", false);
+    }
+  }
+
+  [HarmonyPatch(typeof(QualityUtility), "SendCraftNotification")]
+  class SarcophagusPatch
+  {
+    [HarmonyPrefix]
+    static bool SendCraftNotification(Thing thing, Pawn worker)
+    {
+      if (worker == null)
+      {
+        return false;
+      }
+      CompQuality compQuality = thing.TryGetComp<CompQuality>();
+      if (compQuality == null)
+      {
+        return false;
+      }
+      CompArt compArt = thing.TryGetComp<CompArt>();
+      if (compArt == null || thing is Building_Grave)
+      {
+        if (compQuality.Quality == QualityCategory.Masterwork)
+        {
+          Find.LetterStack.ReceiveLetter("LetterCraftedMasterworkLabel".Translate(), "LetterCraftedMasterworkMessage".Translate(worker.LabelShort, thing.LabelShort, worker.Named("WORKER"), thing.Named("CRAFTED")), LetterDefOf.PositiveEvent, thing, null, null, null, null);
+          return false;
+        }
+        if (compQuality.Quality == QualityCategory.Legendary)
+        {
+          Find.LetterStack.ReceiveLetter("LetterCraftedLegendaryLabel".Translate(), "LetterCraftedLegendaryMessage".Translate(worker.LabelShort, thing.LabelShort, worker.Named("WORKER"), thing.Named("CRAFTED")), LetterDefOf.PositiveEvent, thing, null, null, null, null);
+          return false;
+        }
+      }
+      else
+      {
+        if (compQuality.Quality == QualityCategory.Masterwork)
+        {
+          Find.LetterStack.ReceiveLetter("LetterCraftedMasterworkLabel".Translate(), "LetterCraftedMasterworkMessageArt".Translate(compArt.GenerateImageDescription(), worker.LabelShort, thing.LabelShort, worker.Named("WORKER"), thing.Named("CRAFTED")), LetterDefOf.PositiveEvent, thing, null, null, null, null);
+          return false;
+        }
+        if (compQuality.Quality == QualityCategory.Legendary)
+        {
+          Find.LetterStack.ReceiveLetter("LetterCraftedLegendaryLabel".Translate(), "LetterCraftedLegendaryMessageArt".Translate(compArt.GenerateImageDescription(), worker.LabelShort, thing.LabelShort, worker.Named("WORKER"), thing.Named("CRAFTED")), LetterDefOf.PositiveEvent, thing, null, null, null, null);
+        }
+      }
+      return false;
+    }
+  }
+}
---
+++ dist.bat
@@ -0,0 +1,16 @@
+@echo off
+
+powershell -NoProfile -Command "Remove-Item -Recurse -Path .\dist"
+powershell -NoProfile -Command "New-Item -ItemType Directory -Path .\dist"
+powershell -NoProfile -Command "New-Item -ItemType Directory -Path \".\dist\Sarcophagus Patch\""
+powershell -NoProfile -Command "New-Item -ItemType Directory -Path \".\dist\Sarcophagus Patch\About\""
+powershell -NoProfile -Command "Copy-Item -Recurse -Path .\About\About.xml \".\dist\Sarcophagus Patch\About\""
+powershell -NoProfile -Command "Copy-Item -Recurse -Path .\About\Manifest.xml \".\dist\Sarcophagus Patch\About\""
+powershell -NoProfile -Command "Copy-Item -Recurse -Path .\Assemblies \".\dist\Sarcophagus Patch\""
+powershell -NoProfile -Command "Copy-Item -Recurse -Path .\Source \".\dist\Sarcophagus Patch\""
+powershell -NoProfile -Command "Remove-Item -Recurse -Path \".\dist\Sarcophagus Patch\Source\obj\""
+powershell -NoProfile -Command "Remove-Item -Recurse -Path \".\dist\Sarcophagus Patch\Source\Connected Services\""
+powershell -NoProfile -Command "Remove-Item -Path \".\dist\Sarcophagus Patch\Source\Sarcophagus_Patch.csproj.user\""
+powershell -NoProfile -Command "Get-ChildItem \".\dist\Sarcophagus Patch\Source\" -Recurse -Hidden | Remove-Item -Recurse -Verbose -Force"
+powershell -NoProfile -Command "Copy-Item -Recurse -Path .\LICENSE \".\dist\Sarcophagus Patch\""
+powershell -NoProfile -Command "Copy-Item -Recurse -Path .\Sarcophagus_Patch.sln \".\dist\Sarcophagus Patch\""
