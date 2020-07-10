904ac2461f4461fc864bbd0ecd1d1b3542d2c742
thakyZ <thakyz@outlook.com>
2020-07-10 12:26:41 -0700

    Renamed the Binary

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sarcophagus Patch")]
+[assembly: AssemblyTitle("SarcophagusPatch")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sarcophagus_Patch")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("SarcophagusPatch")]
+[assembly: AssemblyCopyright("Copyright © Nick Mullally 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
